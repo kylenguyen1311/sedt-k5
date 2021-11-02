2f26050197ff0b9efd7ab389d19466e8e74fddaa
kylenguyen1311 <kylenguyen1311@gmail.com>
2021-11-02 14:22:06 +0700

    submit hw5

--- src/lesson1/week1_hw5.java
+++ src/lesson1/week1_hw5.java
@@ -1,4 +1,16 @@
 package lesson1;
 
+import java.util.Scanner;
+
 public class week1_hw5 {
+    public static void main(String[] args) {
+        int a, b = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please input: ");
+        a = in.nextInt();
+        for (int i = 1; i <= a; i++) {
+            b = b*i;
+        }
+        System.out.println(a + "! = " + b);
+    }
 }
